01688cc681b2a7c817d620d8cc369f8e3db31844
Adam Basile <basile.adam@gmail.com>
2024-12-05 18:33:14 +1100

    day 4 part two done

--- src/day04.rs
+++ src/day04.rs
@@ -6,7 +6,48 @@ pub(crate) fn day04(lines: Vec<String>) -> (i32, i32) {
         .iter()
         .map(|cand| re.find_iter(&cand).count() as i32)
         .sum();
-    (partone, -1)
+
+    let re_parttwo = Regex::new(r"MMSS|MSSM|SSMM|SMMS").unwrap();
+    let mut parttwo = 0;
+    for (row, line) in (&lines).iter().enumerate() {
+        let row = row as i32;
+        for (col, c) in line.chars().enumerate() {
+            if c != 'A' {
+                continue;
+            }
+            let col = col as i32;
+            let Some(top_left) = get_char(&lines, row - 1, col - 1) else {
+                continue;
+            };
+            let Some(bottom_left) = get_char(&lines, row + 1, col - 1) else {
+                continue;
+            };
+            let Some(bottom_right) = get_char(&lines, row + 1, col + 1) else {
+                continue;
+            };
+            let Some(top_right) = get_char(&lines, row - 1, col + 1) else {
+                continue;
+            };
+            let test: String = [top_left, bottom_left, bottom_right, top_right]
+                .iter()
+                .collect();
+            if re_parttwo.is_match(&test) {
+                parttwo += 1
+            }
+        }
+    }
+    (partone, parttwo)
+}
+
+fn get_char(lines: &Vec<String>, row: i32, col: i32) -> Option<char> {
+    if row < 0 || col < 0 {
+        return None;
+    }
+    let line = match lines.get(row as usize) {
+        None => return None,
+        Some(line) => line,
+    };
+    line.chars().nth(col as usize)
 }
 
 fn create_candidates(lines: &Vec<String>) -> Vec<String> {
@@ -48,7 +89,7 @@ fn create_candidates(lines: &Vec<String>) -> Vec<String> {
             if row >= height || col as i32 >= width || row < 0 {
                 continue;
             }
-            cand.push(lines[row as usize].chars().nth(col as usize).unwrap());
+            cand.push(lines[row as usize].chars().nth(col).unwrap());
         }
         candidates.push(cand);
     }
@@ -69,13 +110,19 @@ mod tests {
     #[test]
     fn test_day_04_partone_small() {
         let lines = read_testfile("day04testpartone.txt");
-        assert_eq!(day04(lines), (18, 0));
+        assert_eq!(day04(lines), (18, 9));
     }
 
     #[test]
     fn test_day_04_parttwo_small() {
         let lines = read_testfile("day04testparttwo.txt");
         assert_eq!(day04(lines), (0, 9));
+    }
+
+    #[test]
+    fn test_day_04() {
+        let lines = read_testfile("day04.txt");
+        assert_eq!(day04(lines), (2685, 2048));
     }
 
     #[test]
