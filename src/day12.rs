f8aaa4736d57c8bfa9d60910c1b651df4974eab5
Adam Basile <adam.basile@readytech.io>
2024-12-12 20:32:12 +1100

    Day 12 done

--- src/day12.rs
+++ src/day12.rs
@@ -1,35 +1,98 @@
 use std::collections::{HashMap, HashSet};
 
+type Plot = (i32, i32);
+
+#[derive(Eq, Hash, PartialEq, Debug)]
+struct Border {
+    inside: Plot,
+    outside: Plot,
+}
+
+impl Border {
+    fn next_straight(&self) -> Self {
+        let x_diff = self.outside.0 - self.inside.0;
+        let y_diff = self.outside.1 - self.inside.1;
+
+        let (x_delta, y_delta) = match (x_diff, y_diff) {
+            (0, 1) => (1, 0),
+            (1, 0) => (0, -1),
+            (0, -1) => (-1, 0),
+            (-1, 0) => (0, 1),
+            _ => panic!(),
+        };
+        Border {
+            inside: (self.inside.0 + x_delta, self.inside.1 + y_delta),
+            outside: (self.outside.0 + x_delta, self.outside.1 + y_delta),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Stats {
     area: u32,
     perimeter: u32,
 }
 
-fn neighbours(&(h, w): &(i32, i32)) -> [(i32, i32); 4] {
+fn neighbours(&(h, w): &Plot) -> [Plot; 4] {
     [(h - 1, w), (h, w - 1), (h + 1, w), (h, w + 1)]
 }
 pub(crate) fn day12(lines: Vec<String>) -> (i64, i64) {
     let plots = parse(lines);
-    let region_map: HashMap<(i32, i32), u32> = get_region_map(plots);
-    let region_stats = calculate_stats(region_map);
+    let region_map: HashMap<Plot, u32> = get_region_map(&plots);
+    let region_stats = calculate_stats(&region_map);
+    let region_sides = get_sides_per_region(&region_map);
 
     let partone = region_stats
         .values()
         .map(|stats| stats.area * stats.perimeter)
         .sum::<u32>() as i64;
-    let parttwo = 0;
+    let parttwo = region_stats
+        .iter()
+        .map(|(region, stats)| stats.area * region_sides.get(region).unwrap())
+        .sum::<u32>() as i64;
     (partone, parttwo)
 }
 
-fn calculate_stats(region_map: HashMap<(i32, i32), u32>) -> HashMap<u32, Stats> {
+fn get_sides_per_region(region_map: &HashMap<Plot, u32>) -> HashMap<u32, u32> {
+    let mut region_borders: HashMap<u32, HashSet<Border>> = HashMap::new();
+    for (plot, region) in (&region_map).iter() {
+        for &neighbour in neighbours(plot)
+            .iter()
+            .filter(|neighbour| region_map.get(neighbour) != Some(region))
+        {
+            region_borders
+                .entry(*region)
+                .or_insert(HashSet::new())
+                .insert(Border {
+                    inside: *plot,
+                    outside: neighbour,
+                });
+        }
+    }
+
+    let region_sides: HashMap<u32, u32> = region_borders
+        .iter()
+        .map(|(&region, borders)| {
+            (
+                region,
+                borders
+                    .iter()
+                    .filter(|border| !borders.contains(&border.next_straight()))
+                    .count() as u32,
+            )
+        })
+        .collect();
+    region_sides
+}
+
+fn calculate_stats(region_map: &HashMap<Plot, u32>) -> HashMap<u32, Stats> {
     let mut region_stats: HashMap<u32, Stats> = HashMap::new();
     for (plot, region) in region_map.iter() {
         let additional_perimeter = neighbours(plot)
             .iter()
             .filter(|neighbour| region_map.get(neighbour) != Some(region))
             .count() as u32;
-        let mut stats = region_stats.entry(*region).or_insert(Stats {
+        let stats = region_stats.entry(*region).or_insert(Stats {
             area: 0,
             perimeter: 0,
         });
@@ -39,11 +102,11 @@ fn calculate_stats(region_map: HashMap<(i32, i32), u32>) -> HashMap<u32, Stats> 
     region_stats
 }
 
-fn get_region_map(plots: HashMap<(i32, i32), char>) -> HashMap<(i32, i32), u32> {
+fn get_region_map(plots: &HashMap<Plot, char>) -> HashMap<Plot, u32> {
     let mut region_num = 0;
-    let mut region_map: HashMap<(i32, i32), u32> = HashMap::new();
-    let mut visited = HashSet::<(i32, i32)>::new();
-    let mut to_visit: Vec<(i32, i32)> = vec![*plots.keys().next().unwrap()];
+    let mut region_map: HashMap<Plot, u32> = HashMap::new();
+    let mut visited = HashSet::<Plot>::new();
+    let mut to_visit: Vec<Plot> = vec![*plots.keys().next().unwrap()];
     while let Some(master_plot) = to_visit.pop() {
         if visited.contains(&master_plot) {
             continue;
@@ -77,7 +140,7 @@ fn get_region_map(plots: HashMap<(i32, i32), char>) -> HashMap<(i32, i32), u32> 
     region_map
 }
 
-fn parse(lines: Vec<String>) -> HashMap<(i32, i32), char> {
+fn parse(lines: Vec<String>) -> HashMap<Plot, char> {
     let plots: HashMap<_, _> = lines
         .iter()
         .enumerate()
@@ -100,12 +163,25 @@ mod tests {
     #[test]
     fn test_day_12_small() {
         let lines = read_testfile("day12test.txt");
-        assert_eq!(day12(lines), (1930, 0));
+        assert_eq!(day12(lines), (1930, 1206));
     }
 
     #[test]
     fn test_day_12() {
         let lines = read_testfile("day12.txt");
-        assert_eq!(day12(lines), (1533644, 0));
+        assert_eq!(day12(lines), (1533644, 936718));
+    }
+
+    #[test]
+    fn test_next() {
+        let border = Border {
+            inside: (0, 1),
+            outside: (0, 0),
+        };
+        let expected = Border {
+            inside: (1, 1),
+            outside: (1, 0),
+        };
+        assert_eq!(border.next_straight(), expected);
     }
 }
