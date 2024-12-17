eafc619d41ca1a4922fc98a8b0f8e6e71d6f7dec
Adam Basile <adam.basile@readytech.io>
2024-12-18 09:42:47 +1100

    Day 17 part way

--- src/day17.rs
+++ src/day17.rs
@@ -1,7 +1,7 @@
 use crate::FunctionOutput;
 use crate::FunctionOutput::StringPair;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct StrangeDevice {
     a: i64,
     b: i64,
@@ -9,6 +9,13 @@ struct StrangeDevice {
 }
 
 impl StrangeDevice {
+    fn new_with_a(&self, a: i64) -> Self {
+        Self {
+            a,
+            b: self.b,
+            c: self.c,
+        }
+    }
     fn combo(&self, operand: &i64) -> i64 {
         self.resolve_combo(operand.try_into().unwrap())
     }
@@ -53,9 +60,66 @@ enum Register {
 }
 
 pub(crate) fn day17(lines: Vec<String>) -> FunctionOutput {
-    let (mut device, instructions) = parse(lines);
-    println!("{:?}", instructions);
+    let (device, instructions) = parse(lines);
 
+    let output = compute(device, &instructions, false).unwrap();
+
+    let partone = output
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join(",");
+
+    // let parttwo = find_quine(device, &instructions);
+    let parttwo = "".to_string();
+    StringPair(partone, parttwo)
+}
+
+fn find_quine(device: StrangeDevice, instructions: &Vec<i64>) -> String {
+    todo!("doesn't work");
+    // one jump to the start and it's at the end
+    assert_eq!(
+        vec![instructions.len() - 2],
+        instructions
+            .iter()
+            .enumerate()
+            .filter(|(i, &v)| ((i % 2) == 0) && v == 3)
+            .map(|(i, _)| i)
+            .collect::<Vec<_>>()
+    );
+    assert_eq!(instructions.last().unwrap().clone(), 0);
+
+    let outputs_per_loop = instructions
+        .iter()
+        .enumerate()
+        .filter(|(i, &v)| ((i % 2) == 0) && v == 5)
+        .count();
+
+    let divides_per_loop = instructions
+        .iter()
+        .enumerate()
+        .filter(|(i, &v)| ((i % 2) == 0) && v == 0)
+        .count();
+
+    let max_power_of_8 = (instructions.len() * divides_per_loop) / outputs_per_loop;
+
+    let parttwo = (8_i64.pow((max_power_of_8 - 1) as u32)..8_i64.pow((max_power_of_8) as u32))
+        .filter(|&a| {
+            let new_device = device.new_with_a(a);
+            let output = compute(new_device, &instructions, true);
+            match output {
+                Some(_) => true,
+                None => false,
+            }
+        })
+        .next()
+        .unwrap()
+        .to_string();
+    parttwo
+}
+
+fn compute(device: StrangeDevice, instructions: &Vec<i64>, try_to_match: bool) -> Option<Vec<i64>> {
+    let mut device = device.clone();
     let mut output = Vec::new();
     let mut instruction_pointer: i64 = 0;
     while let (Some(opcode), Some(operand)) = (
@@ -73,18 +137,23 @@ pub(crate) fn day17(lines: Vec<String>) -> FunctionOutput {
                 }
             }
             4 => device.b = device.b ^ device.c,
-            5 => output.push(device.combo(operand) % 8),
+            5 => {
+                let val = device.combo(operand) % 8;
+                if try_to_match && (Some(&val) != instructions.get(output.len())) {
+                    return None;
+                }
+                output.push(val);
+            }
             6 => device.b = device.divide(operand),
             7 => device.c = device.divide(operand),
             _ => panic!(),
         }
         instruction_pointer = jump_to;
     }
-    let output: Vec<_> = output.iter().map(|x| x.to_string()).collect();
-
-    let partone = output.join(",");
-    let parttwo = String::new();
-    StringPair(partone, parttwo)
+    if try_to_match && (output.len() != instructions.len()) {
+        return None;
+    }
+    Some(output)
 }
 
 fn parse(lines: Vec<String>) -> (StrangeDevice, Vec<i64>) {
@@ -106,11 +175,19 @@ mod tests {
     use crate::read_testfile;
 
     #[test]
-    fn test_day_17_small() {
-        let lines = read_testfile("day17test.txt");
+    fn test_day_17_small1() {
+        let lines = read_testfile("day17test1.txt");
         assert_eq!(
             day17(lines),
-            StringPair("4,6,3,5,6,3,5,2,1,0".to_string(), "".to_string())
+            StringPair("4,6,3,5,6,3,5,2,1,0".to_string(), "117440".to_string()),
+        );
+    }
+    #[test]
+    fn test_day_17_small2() {
+        let lines = read_testfile("day17test2.txt");
+        assert_eq!(
+            day17(lines),
+            StringPair("5,7,3,0".to_string(), "117440".to_string()),
         );
     }
 
